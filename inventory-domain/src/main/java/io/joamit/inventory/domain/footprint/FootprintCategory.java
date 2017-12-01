344727fd0f111b62446c3d2606c1ef7788bc26a7
Amit Joshi <amitjoshiave@gmail.com>
2017-12-01 21:44:30 +0100

    footprint category domain java doc updates

--- inventory-domain/src/main/java/io/joamit/inventory/domain/footprint/FootprintCategory.java
+++ inventory-domain/src/main/java/io/joamit/inventory/domain/footprint/FootprintCategory.java
@@ -3,20 +3,41 @@ package io.joamit.inventory.domain.footprint;
 import io.joamit.inventory.domain.category.Category;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FootprintCategory extends Category {
 
+    /**
+     * Parent category for this Footprint Cateogry (if there is any) (optional)
+     */
     @DBRef
     private Category parent;
 
+    /**
+     * Children categories for this Footprint category (if there are any) (totally optional)
+     */
     @DBRef
     private List<Category> children;
 
+    /**
+     * list of footprint this category holds
+     * (this is a reference to all the footprints that have this category as a footprint category)
+     */
     @DBRef
     private List<Footprint> footprints;
 
+    /**
+     * Full category path from its root parent
+     */
     private String categoryPath;
+
+    /**
+     * Initialize the category with empty children list
+     */
+    public FootprintCategory() {
+        this.children = new ArrayList<>();
+    }
 
     public Category getParent() {
         return parent;
