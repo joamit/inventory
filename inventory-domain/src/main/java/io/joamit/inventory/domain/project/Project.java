8a99c8cc0b52b272c0bf4fb63aab328575647087
Amit Joshi <amitjoshiave@gmail.com>
2017-12-01 23:08:06 +0100

    inventory project domain java doc updates

--- inventory-domain/src/main/java/io/joamit/inventory/domain/project/Project.java
+++ inventory-domain/src/main/java/io/joamit/inventory/domain/project/Project.java
@@ -5,22 +5,56 @@ import io.joamit.inventory.domain.part.Part;
 import io.joamit.inventory.domain.user.User;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Holds information about the project
+ */
 public class Project extends BaseDocument {
 
+    /**
+     * Project name
+     */
     private String name;
 
+    /**
+     * User who created the project
+     */
     @DBRef
     private User user;
 
+    /**
+     * Parts that this project needs or has reference to
+     */
     @DBRef
     private List<Part> parts;
 
+    /**
+     * Project description
+     */
     private String description;
 
+    /**
+     * Project attachments
+     */
     @DBRef
     private List<ProjectAttachment> projectAttachments;
+
+    /**
+     * Initialize a project with given details
+     *
+     * @param name        of the project
+     * @param user        creating the project
+     * @param description of the project
+     */
+    public Project(String name, User user, String description) {
+        this.name = name;
+        this.user = user;
+        this.description = description;
+        this.parts = new ArrayList<>();
+        this.projectAttachments = new ArrayList<>();
+    }
 
     public String getName() {
         return name;
--- inventory-domain/src/main/java/io/joamit/inventory/domain/project/ProjectAttachment.java
+++ inventory-domain/src/main/java/io/joamit/inventory/domain/project/ProjectAttachment.java
@@ -3,12 +3,34 @@ package io.joamit.inventory.domain.project;
 import io.joamit.inventory.domain.BaseDocument;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 
+/**
+ * Holds a project attachment information, can be extended to hold attachment storage details
+ */
 public class ProjectAttachment extends BaseDocument {
 
+    /**
+     * Reference to the project that has this attachment
+     */
     @DBRef
     private Project project;
 
+    /**
+     * Name of the attachment
+     */
     private String name;
+
+    /**
+     * Details on the project attachment
+     */
+    private String details;
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
 
     public Project getProject() {
         return project;
