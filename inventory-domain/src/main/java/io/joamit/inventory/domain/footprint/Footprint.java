9b29af7e38bc6b039d6df81f887535c85c7489de
Amit Joshi <amitjoshiave@gmail.com>
2017-12-01 21:36:55 +0100

    footprint domain java doc updates

--- inventory-domain/src/main/java/io/joamit/inventory/domain/footprint/Footprint.java
+++ inventory-domain/src/main/java/io/joamit/inventory/domain/footprint/Footprint.java
@@ -1,24 +1,63 @@
 package io.joamit.inventory.domain.footprint;
 
 import io.joamit.inventory.domain.BaseDocument;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Footprint extends BaseDocument {
 
+    /**
+     * Holds the part footprint name
+     */
     private String name;
 
+    /**
+     * Holds the footprint description
+     */
     private String description;
 
+    /**
+     * Holds the reference to the footprint category details
+     */
+    @DBRef
     private FootprintCategory footprintCategory;
 
+    /**
+     * Holds the reference to part footprint image details
+     */
+    @DBRef
     private FootprintImage footprintImage;
 
+    /**
+     * Holds information about the part footprint attachments
+     */
+    @DBRef
     private List<FootprintAttachment> footprintAttachments;
 
     public String getCategoryPath() {
         if (this.footprintCategory == null) return "";
         else return this.footprintCategory.getCategoryPath();
+    }
+
+    /**
+     * Initialize a new part footprint with empty list of attachments
+     *
+     * @param name        of the footprint
+     * @param description of the footprint
+     */
+    public Footprint(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.footprintAttachments = new ArrayList<>();
+    }
+
+    /**
+     * Default constructor with initialized attachments list
+     */
+    public Footprint() {
+        this.footprintAttachments = new ArrayList<>();
     }
 
     public String getName() {
@@ -59,5 +98,12 @@ public class Footprint extends BaseDocument {
 
     public void setFootprintAttachments(List<FootprintAttachment> footprintAttachments) {
         this.footprintAttachments = footprintAttachments;
+    }
+
+    public void addAttachment(FootprintAttachment footprintAttachment) {
+        if (footprintAttachment != null) {
+            footprintAttachment.setFootprint(this);
+        }
+        this.footprintAttachments.add(footprintAttachment);
     }
 }
