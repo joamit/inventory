5048673f48230e7d625d95d6c1ed3ea060512dab
Amit Joshi <amitjoshiave@gmail.com>
2017-12-01 22:33:52 +0100

    inventory user provider domain java doc updates

--- inventory-domain/src/main/java/io/joamit/inventory/domain/user/UserProvider.java
+++ inventory-domain/src/main/java/io/joamit/inventory/domain/user/UserProvider.java
@@ -2,11 +2,25 @@ package io.joamit.inventory.domain.user;
 
 import io.joamit.inventory.domain.BaseDocument;
 
+/**
+ * Holds information about the user type
+ */
 public class UserProvider extends BaseDocument {
 
+    /**
+     * Type of user i.e. internal/external/manufacturer/distributor
+     */
     private String type;
 
+    /**
+     * If user information is editable or not
+     */
     private Boolean editable;
+
+    public UserProvider(String type, Boolean editable) {
+        this.type = type;
+        this.editable = editable;
+    }
 
     public String getType() {
         return type;
