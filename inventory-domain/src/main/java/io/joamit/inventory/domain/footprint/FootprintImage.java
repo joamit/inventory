1de0024d007737b976c1615f67c076ef48204d45
Amit Joshi <amitjoshiave@gmail.com>
2017-12-01 21:46:40 +0100

    footprint image domain java doc updates

--- inventory-domain/src/main/java/io/joamit/inventory/domain/footprint/FootprintImage.java
+++ inventory-domain/src/main/java/io/joamit/inventory/domain/footprint/FootprintImage.java
@@ -5,10 +5,32 @@ import org.springframework.data.mongodb.core.mapping.DBRef;
 
 public class FootprintImage extends BaseDocument {
 
+    /**
+     * Reference to the footprint object of which this is an image
+     */
     @DBRef
     private Footprint footprint;
 
+    /**
+     * Name of the footprint image
+     */
     private String name;
+
+    /**
+     * Image comments (Optional)
+     */
+    private String comments;
+
+    /**
+     * Initialize a footprint image
+     *
+     * @param name     of the image
+     * @param comments for the image
+     */
+    public FootprintImage(String name, String comments) {
+        this.name = name;
+        this.comments = comments;
+    }
 
     public String getName() {
         return name;
@@ -24,5 +46,13 @@ public class FootprintImage extends BaseDocument {
 
     public void setFootprint(Footprint footprint) {
         this.footprint = footprint;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
     }
 }
